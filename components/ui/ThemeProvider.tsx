e516674b51d54d9e9d0d86d96b75c3d880034bdc
betomoedano <betomoedano@outlook.com>
2025-02-19 15:59:02 -0600

    fix theme

--- components/ui/ThemeProvider.tsx
+++ components/ui/ThemeProvider.tsx
@@ -32,7 +32,7 @@ import { useColorScheme } from "react-native";
 // };
 
 export default function ThemeProvider(props: { children: React.ReactNode }) {
-  const colorScheme = process.env.EXPO_OS === "web" ? "dark" : useColorScheme();
+  const colorScheme = useColorScheme();
   return (
     <RNTheme
       // This isn't needed on iOS or web, but it's required on Android since the dynamic colors are broken
